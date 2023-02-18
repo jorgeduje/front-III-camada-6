daeb7cadb90f9c0e2687987dfc469252884d3f35
jorgeduje <jorgeduje4@gmail.com>
2023-02-17 23:34:01 -0300

    cambios en app

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className="read-the-docs">
+        Hola
+      </p>
     </div>
   )
 }
