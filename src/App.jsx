f75fa09de53b9c41fe9ef20b8837ef4425599122
jorgeduje <jorgeduje4@gmail.com>
2023-03-03 00:18:25 -0300

    formularios y eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-import './App.css'
+import "./App.css";
+import Form from "./Form/Form";
 
 function App() {
-
   return (
     <div className="App">
-      <h1>App</h1>
+     <Form />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
