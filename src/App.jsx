7604fda06b59ccc897c33174d20e91312b33d94e
jorgeduje <jorgeduje4@gmail.com>
2023-03-07 23:02:14 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import "./App.css";
 
 import "./App.css";
 import Form from "./Components/Form/Form";
-import ItemsList from "./Components/ItemsList/ItemsList";
-import Select from "./Components/Select/Select";
 
 function App() {
   return (
-    <div className="App">
-      <ItemsList />
+    <div>
 
       <Form />
-      <Select />
     </div>
   );
 }
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,74 +1,24 @@
-import { useState } from "react";
-
-//onChange
-//onSubmit
+import "./Form.css";
 
 const Form = () => {
-  const [data, setData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const [flag, setFlag] = useState(true);
-
-  const handleChangeName = (e) => {
-    setData({ ...data, email: e.target.value });
-  };
-
-  const validateUserName = (str) => {
-    // " jorge" vs "jorge"
-    const withoutSpaces = str.trim();
-
-    if (withoutSpaces.length > 2 && str === withoutSpaces) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const emailIsValid = validateUserName(data.email);
-    const passwordIsValid = true;
-
-    if (!emailIsValid && !isPasword) {
-      alert("los datos son incorrectos");
-      return;
-    }
-
-    // LO ULTIMO
-    alert(`su email es : ${data.email} y su contraseña: ${data.password} `);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === " ") {
-      e.preventDefault();
-      alert("no se permiten espacion vacios");
-    }
-  };
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="userMail"
-          onChange={handleChangeName}
-          //   onKeyDown={handleKeyDown}
-        />
-        <input
-          type="text"
-          name="userPassword"
-          onChange={(e) => setData({ ...data, password: e.target.value })}
-        />
+    <div className="container">
+      <form className="form">
+        <input type="text" name="email" />
+        <input type="text" name="password" />
+        <select
+          style={{
+            width: "100%",
+            height: "30px",
+            fontSize: "0.9rem",
+          }}
+        >
+          <option value="" default>
+            no seleccionado
+          </option>
+        </select>
         <button type="submit">Ingresar</button>
       </form>
-
-      {
-        // flag  ? <h1>Me muestro siempre</h1> : null
-        flag && <h1>Me muestro siempre</h1>
-      }
     </div>
   );
 };
