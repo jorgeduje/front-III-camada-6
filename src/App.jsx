b7d99f93388029b0bee8cc1b9a8acf489b4ab391
jorgeduje <jorgeduje4@gmail.com>
2023-03-17 00:15:30 -0300

    se trabajo con axios y verbos http, repaso de useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-
-import { useState } from 'react'
-import './App.css'
-import Counter from './Components/Counter/Counter'
-import Products from './Components/Products/Products'
+import "./App.css";
+import ProductsContainer from "./Components/Products/ProductsContainer";
 
 function App() {
-
-  const [show, setShow] = useState(false)
-
   return (
     <div className="App">
-      {/* <button onClick={()=>setShow(!show)}>Montar y desmontar componente</button>
-      {
-        show ? <Counter /> : <h1>No esta</h1>
-      } */}
-
-      <Products />
+      <ProductsContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
