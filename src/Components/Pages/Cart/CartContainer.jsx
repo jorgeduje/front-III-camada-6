ad8706b1f3e551727ea1459476c12149a5b67831
jorgeduje <jorgeduje4@gmail.com>
2023-04-11 22:51:03 -0300

    minor fix, border repet

--- src/Components/Pages/Cart/CartContainer.jsx
+++ src/Components/Pages/Cart/CartContainer.jsx
@@ -58,7 +58,6 @@ const CartContainer = () => {
             style={{
               width: "100%",
               height: "100px",
-              border: "2px solid white",
               display: "flex",
               justifyContent: "space-between",
               alignItems: "center",
